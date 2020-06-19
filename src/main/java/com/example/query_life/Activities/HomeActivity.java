bf9dbffe0e44f74e51adf833d3b4302374e89959
NalinGupta0 <48588594+NalinGupta0@users.noreply.github.com>
2020-06-19 16:25:38 +0530

    Add files via upload

---
+++ src/main/java/com/example/query_life/Activities/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.example.query_life.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import  com.example.query_life.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
